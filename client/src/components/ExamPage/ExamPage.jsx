c9c84cd6d9e37a4d5e025e560bf2d5ca18fea299
Deep Patel <deep1907rp@gmail.com>
2024-02-22 00:03:12 +0530

    ML integration done

--- client/src/components/ExamPage/ExamPage.jsx
+++ client/src/components/ExamPage/ExamPage.jsx
@@ -9,33 +9,106 @@ export default function ExamPage({examid,studentid}) {
   const webcamRef = React.useRef(null);
   const [imgSrc, setImgSrc] = React.useState(null);
   const [queryParameters] = useSearchParams();
-  const [cam,setCam] = useState(false);
+  const [cam,setCam] = useState(true);
+  const [stuimage, setStuImage] = useState("");
+  const [multipleperson,setMultiplePerson]=useState(0);
+  const [differentperson,setDifferentPerson]=useState(0);
 
   const capture = React.useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    setImgSrc(imageSrc);
-    console.log(imageSrc);
+    console.log(stuimage.length)
+    if (webcamRef.current && stuimage.length>0) {
+      const imageSrc = webcamRef.current.getScreenshot();
+      // setImgSrc(imageSrc);
+      console.log("heioiy");
+    // console.log(imageSrc);
+      console.log(typeof(stuimage))
+      console.log(stuimage.length)
+      console.log(typeof(imageSrc))
+      axios.post(`http://127.0.0.1:8000/check`,
+          {"photo":stuimage,
+          "webcam":imageSrc
+          }).
+      then((res)=>{
+        console.log("fef");
+        if(res.data.no_of_person>1){
+          setMultiplePerson(multipleperson+1);
+        }
+        if(res.data.verified==false){
+          setDifferentPerson(differentperson+1);
+        }
+        console.log(res);
+      }).catch((err)=>{
+        console.log("feefesfes")
+        console.log(err);
+      });
+    }
+    console.log("hey");
   }, [webcamRef, setImgSrc]);
-  
+
+  function capture2(){
+    if (webcamRef.current) {
+      const imageSrc = webcamRef.current.getScreenshot();
+      // setImgSrc(imageSrc);
+      console.log("heioiy");
+    // console.log(imageSrc);
+      console.log(typeof(stuimage))
+      console.log(stuimage.length)
+      
+      console.log(typeof(imageSrc))
+      axios.post(`http://127.0.0.1:8000/check`,
+          {"photo":stuimage,
+          "webcam":imageSrc
+          }).
+      then((res)=>{
+        console.log("fef");
+        console.log(res);
+      }).catch((err)=>{
+        console.log("feefesfes")
+        console.log(err);
+      });
+    }
+    console.log("hey");
+  }
   function camswitch(){
     setCam(!cam);
   }
 
   var stuid="jee";
   console.log(stuid);
-  var stuimage;
+
+
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_BACKEND}/student/getimage/${stuid}`,).then((res)=>{
-      stuimage = res.data;
+    axios.get(`${process.env.REACT_APP_BACKEND}/student/getimage/${stuid}`,).
+    then((res)=>{
+      // stuimage = res.data;
+      setStuImage(res.data);
       console.log("fasd1"); 
-      console.log(stuimage);
+      // console.log(res.data);
+      console.log(res.data.length)
+      console.log(stuimage.length)
     }).catch((err)=>{
       console.log(err);
     });
-  }, []);
+  },[]);
+
 
   
-  
+  useEffect(() => {
+    
+    if (cam) {
+        setTimeout(()=>{
+          if(stuimage.length!=0){
+            console.log("------")
+            console.log(stuimage.length)
+            console.log("////////////")
+            const intervalId = setInterval(() => {
+              capture2();
+            }, 8000);
+            return () => clearInterval(intervalId);
+          }
+      },3000)      
+    }
+  }, [cam, capture,stuimage]);
 
   useEffect(() => {
     const keyDownHandler = event => {
@@ -48,6 +121,9 @@ export default function ExamPage({examid,studentid}) {
       document.removeEventListener('keydown', keyDownHandler);
     };
   }, []);
+  // setTimeout(() => {
+  //   setCam(true);
+  // }, 3000);
 
   useRestrictCopyPaste({window, actions:["copy","cut","paste","select"]})
   return (
@@ -67,11 +143,9 @@ export default function ExamPage({examid,studentid}) {
           forceScreenshotSourceSize={true}
           style={{width: "0%"}}
           // screenshotQuality={1}
-        />
-
-        {/* {axios.post(`process.env.SERVICE_URL`, {"image":{imgSrc}}).then()} */}
-        
+        />        
       </div> :""}
+      
       
       <button onClick={capture}>Capture photo</button>
       {imgSrc && (
--- service/main.py
+++ service/main.py
@@ -1,27 +1,51 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Union
 from pydantic import BaseModel
 from yolo5face.get_model import get_model
 from deepface import DeepFace
 import cv2
+import base64
 import numpy as np
 
 model = get_model("yolov5n", device=0, min_face=24)
 app = FastAPI()
-    
+
+origins = [
+    "http://localhost:3001",# FastAPI address
+    "http://127.0.0.1:3001",
+    "http://localhost:3001/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Body(BaseModel):
+    photo: str
+    webcam: str 
+        
 @app.post("/check")
-def check(file: UploadFile = File(...), file2: UploadFile = File(...)):
-    contents1 = file.file.read()
-    contents = cv2.imdecode(np.fromstring(contents1, np.uint8), cv2.IMREAD_COLOR)
+def check(body: Body):
+    # print(body)
+    base64_data = body.webcam.split(",")[-1]  # Remove the base64 image format prefix if exists
+    contents1 = base64.b64decode(base64_data)
+    contents = cv2.imdecode(np.frombuffer(contents1, np.uint8), cv2.IMREAD_COLOR)
     contents = cv2.cvtColor(contents, cv2.COLOR_BGR2RGB)
     enhanced_boxes, enhanced_key_points, enhanced_scores = model(contents, target_size=[320, 640, 1280])
     nop = len(enhanced_boxes)
     
-    pass1 = file2.file.read()
-    pass2 = cv2.imdecode(np.fromstring(pass1, np.uint8), cv2.IMREAD_COLOR)
+    # pass1 = file2.file.read()
+    photo=body.photo.split(",")[-1]
+    photo=base64.b64decode(photo)
+    pass2 = cv2.imdecode(np.frombuffer(photo, np.uint8), cv2.IMREAD_COLOR)
     veri=DeepFace.verify(contents, pass2,detector_backend="opencv",model_name="Facenet512",enforce_detection=False,align=True,distance_metric="euclidean_l2")
-    print(veri)
+    # print(veri)
     return {"no_of_person": nop, "verified": veri['verified']}
-
 
 
